ff43f44b5a518c438f70cef8590a9ea5202a6ddb
llittlefoxx <mohamedali.tlili@esprit.tn>
2016-03-21 17:06:43 +0100

    My first Commit

---
+++ TunisiaMall/TunisiaMall/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TunisiaMall.MvcApplication" Language="C#" %>
---
+++ TunisiaMall/TunisiaMall/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TunisiaMall.Startup))]
+namespace TunisiaMall
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
